bdc69beafd681d56609e7e8b4bd9287ee4954a37
tonniegeetz <tonniegeetz@gmail.com>
2024-04-24 06:18:52 +0300

    Understand palindromes and .split, .reverse, and .join methods

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversed = word.split('').reverse().join('');
+
+  return word === reversed;
 }
 
 /* 
   Add your pseudocode here
 */
-
+// reverse the string and put it in a variable. Then compare the reversed variable with the original string. Print true if they are equal and false if they are not equal
 /*
   Add written explanation of your solution here
 */
@@ -13,13 +16,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log('Expecting: true');
+  console.log('=>', isPalindrome('racecar'));
 
-  console.log("");
+  console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('Expecting: false');
+  console.log('=>', isPalindrome('robot'));
 }
 
 module.exports = isPalindrome;
